4d79f5628fb750243faa86f20513c088c684ea0c
ts-miller <tylerstephen11@gmail.com>
2020-10-20 17:23:12 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,8 @@
+class Song < ApplicationRecord
+    validates :title, presence: true
+    validates_uniqueness_of :title, scope: [:artist_name, :release_year]
+    validates :released, inclusion: { in: [true, false] }
+    validates :release_year, presence: true, if: :released?
+    validates :release_year, numericality: { less_than_or_equal_to: Date.current.year }, allow_blank: true
+
+end
